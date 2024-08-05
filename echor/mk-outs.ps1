519d7bcc58d6bddef53709a6d2423fd7a1b877c8
sam <sam@jennifersoft.com>
2024-08-06 08:22:09 +0900

    make expected files using windows echo

--- echor/mk-outs.ps1
+++ echor/mk-outs.ps1
@@ -1,9 +1,9 @@
-$OUTDIR = "tests/expected"
-if (-not (Test-Path -Path $OUTDIR)) {
-    New-Item -ItemType Directory -Path $OUTDIR
+$OUTDIR = "tests\expected"
+if (-not (Test-Path $OUTDIR)) {
+    New-Item -ItemType Directory -Path $OUTDIR | Out-Null
 }
 
-target/debug/echor "Hello there" | Out-File -FilePath "$OUTDIR/hello1.txt"
-target/debug/echor "Hello"  "there" | Out-File -FilePath "$OUTDIR/hello2.txt"
-target/debug/echor -n "Hello  there" | Out-File -FilePath "$OUTDIR/hello1.n.txt"
-target/debug/echor -n "Hello"  "there" | Out-File -FilePath "$OUTDIR/hello2.n.txt"
+"Hello there" | Out-File -FilePath "$OUTDIR\hello1.txt"
+"Hello", "there" -join ' ' | Out-File -FilePath "$OUTDIR\hello2.txt"
+"Hello  there" | Out-File -FilePath "$OUTDIR\hello1.n.txt" -NoNewline
+"Hello", "there" -join ' ' | Out-File -FilePath "$OUTDIR\hello2.n.txt" -NoNewline
