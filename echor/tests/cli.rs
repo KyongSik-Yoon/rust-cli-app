072fc0c22f9884f368ed047f94ef41addaf45356
sam <sam@jennifersoft.com>
2024-08-06 08:16:00 +0900

    conditional line separator

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -21,7 +21,13 @@ fn runs() {
 fn hello1() {
     let outfile = "tests/expected/hello1.txt";
     let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
 
+    #[cfg(windows)]
+    let expected = expected.replace("\r\n", "\n");
+
+    #[cfg(not(windows))]
+    let expected = expected;
+
+    let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("Hello there").assert().success().stdout(expected);
 }
