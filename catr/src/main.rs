ef3762da4f4417d1d96fab82e2a0ff391759344d
sam <sam@jennifersoft.com>
2024-08-12 08:21:57 +0900

    builder 방식 구현 시도 - 옵션에 대한 설명까지

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,49 @@
+use clap::{Arg, ArgAction, Command};
+
+#[derive(Debug)]
+struct Args {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+fn get_args() -> Args {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("sam")
+        .about("Rust version of `cat`")
+        .arg(
+            Arg::new("files")
+                .required(true)
+                .value_name("FILE")
+                .num_args(0..)
+                .help("Input file(s)")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("number_lines")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("number_nonblank_lines")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    Args {
+        files: matches.get_many("files").unwrap().cloned().collect(),
+        number_lines: matches.get_flag("number_lines"),
+        number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_args();
+    println!("{args:#?}");
 }
