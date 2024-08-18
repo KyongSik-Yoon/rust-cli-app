92bf0c56581fc0388ef762ba026a1cd9bc357a6a
sam <sam@jennifersoft.com>
2024-08-18 14:08:35 +0900

    두 방식을 함수형 파라미터를 이용하여 필요에 따라 교체할 수 있도록 하기

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,4 +1,5 @@
-use clap::Parser;
+use anyhow::Result;
+use clap::{Arg, ArgAction, Command, Parser};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
@@ -13,7 +14,51 @@ struct Args {
     number_nonblank_lines: bool,
 }
 
+fn get_args() -> Args {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("sam")
+        .about("Rust version of `cat`")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .num_args(1..)
+                .help("Input file(s)")
+                .default_value("-")
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    Args {
+        files: matches.get_many("files").unwrap().cloned().collect(),
+        number_lines: matches.get_flag("number"),
+        number_nonblank_lines: matches.get_flag("number_nonblank"),
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        println!("{filename}");
+    }
+    Ok(())
+}
+
 fn main() {
-    let args = Args::parse();
-    println!("{args:#?}");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
 }
