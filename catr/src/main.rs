c5881d33cfab6454bb12415c6544a3745710f440
sam <sam@jennifersoft.com>
2024-08-18 12:42:10 +0900

    derive 버전 구현 추가

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,49 +1,19 @@
-use clap::{Arg, ArgAction, Command};
+use clap::{Arg, ArgAction, Command, Parser};
 
-#[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
 struct Args {
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
+
+    #[arg(short('n'), long("number"), conflicts_with("number_nonblank_lines"))]
     number_lines: bool,
+
+    #[arg(short('b'), long("number-nonblank"))]
     number_nonblank_lines: bool,
 }
 
-fn get_args() -> Args {
-    let matches = Command::new("catr")
-        .version("0.1.0")
-        .author("sam")
-        .about("Rust version of `cat`")
-        .arg(
-            Arg::new("files")
-                .value_name("FILE")
-                .num_args(1..)
-                .help("Input file(s)")
-                .default_value("-")
-        )
-        .arg(
-            Arg::new("number_lines")
-                .short('n')
-                .long("number")
-                .help("Number lines")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("number_nonblank_lines")
-                .short('b')
-                .long("number-nonblank")
-                .help("Number non-blank lines")
-                .action(ArgAction::SetTrue),
-        )
-        .get_matches();
-
-    Args {
-        files: matches.get_many("files").unwrap().cloned().collect(),
-        number_lines: matches.get_flag("number_lines"),
-        number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
-    }
-}
-
 fn main() {
-    let args = get_args();
+    let args = Args::parse();
     println!("{args:#?}");
 }
